34a0f9a04fa8a58722c905a794ab40bd3b98f971
NatzzXD <107767373+NatzzXD@users.noreply.github.com>
2022-06-19 12:15:48 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,14 +9,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //Ubah Disini
-global.ownerNumber = ['6285725753532','6285725753532','0']
+global.ownerNumber = ['6281563953229','6285725753532','0']
 global.ownerName = 'Natzzzz X All Creator'
 global.packname = 'Ichigo Kurosaki'
 global.author = 'Whatsapp Bot 2022'
 global.prefa = ['','!','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
 global.sessionName = 'sesi'
 
-//Message Nya Ubah Disini
+//Hallo Semuanya
 global.mess = {
 admin: 'You Are Not Admin!',
 botAdmin: 'Bot Not Admin!',
